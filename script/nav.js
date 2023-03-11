cc4498eb14a43b12e9a60a14a876ba58609e08b3
Lingashkumaar <lingashkumaarm.21aid@kongu.edu>
2023-03-11 13:58:39 +0530

    nav min changed

--- script/nav.js
+++ script/nav.js
@@ -1,1 +1,13 @@
-
+var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "none") {
+          dropdownContent.style.display = "block";
+        } else {
+          dropdownContent.style.display = "none";
+        }
+      });
+    }
